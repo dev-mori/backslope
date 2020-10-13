17659194a6f488863d929bac18127cc1c30bdee3
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-10-13 21:29:58 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_action :sign_in_required
   def show
-    @tweets = Tweet.where(user_id: params[:id])
     @user = User.find_by(id: params[:id])
+    @tweets = Tweet.where(user_id: params[:id])
+    @tweets_count = @tweets.count
+    @follow_count = Follow.where(follow_id: params[:id]).count
+    @follower_count = Follow.where(follower_id: params[:id]).count
   end
 end
